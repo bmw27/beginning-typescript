874c457b1cef0483401dc80069eb9b51b0f92e58
Brandon Williams <bmwill@outlook.com>
2021-06-24 15:53:42 -0700

    wip

--- index.ts
+++ index.ts
@@ -1,44 +1,46 @@
-const greeting: string = 'Hello, Laracasts!';
+const greeting: string = "Hello, Laracasts!";
 
 const year: number = 2021;
 
 const addTwoNumbers = (a: number, b: number): number => a + b;
 
 interface User {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 interface Book {
-    id: string;
-    name: string;
-    releaseDate?: Date;
+  id: string;
+  name: string;
+  releaseDate?: Date;
 }
 
 const getUserById = (id: string): User => ({
-    id,
-    name: "John Doe",
+  id,
+  name: "John Doe",
 });
 
 const getBookById = (id: string): Book => ({
-    id,
-    name: "TypeScript is Best Script!",
-    releaseDate: new Date(),
+  id,
+  name: "TypeScript is Best Script!",
+  releaseDate: new Date(),
 });
 
-const user  = getUserById('uuid-1');
-const book  = getBookById('uuid-1');
+const user = getUserById("uuid-1");
+const book = getBookById("uuid-1");
 
-const saveUser = (user: User) => console.log('saving user', {
+const saveUser = (user: User) =>
+  console.log("saving user", {
     prop1: user.id,
     prop2: user.name,
-});
+  });
 
-const saveBook = (book: Book) => console.log('saving book', {
+const saveBook = (book: Book) =>
+  console.log("saving book", {
     prop1: book.id,
     prop2: book.name,
     prop3: book.releaseDate?.toISOString(),
-});
+  });
 
 saveUser(book);
 saveBook(user);
